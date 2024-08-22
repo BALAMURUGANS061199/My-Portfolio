c6a5f3647754fe9d462e00033a55cce6a498d28d
balamurugans61199 <balamurugans061199@gmail.com>
2024-08-22 15:03:48 +0530

    Portfolio Page Done

--- src/components/UI/Portfolio.jsx
+++ src/components/UI/Portfolio.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import data from "../../assets/data/portfolioData";
+import Modal from "./Modal";
 const Portfolio = () => {
+  const [nextItem, setNextItem] = useState(6);
+  const [portfolios, setPortfolios] = useState(data);
+  const [selectTab, setSelectTab] = useState("all");
+  const [showmodal, setshowmodal] = useState(false);
+  const [activeID, setactiveID] = useState(null);
+
+  const loadMoreHandler = () => {
+    setNextItem((prev) => prev + 3);
+  };
+
+  const showmodalhandler = (id) => {
+    setshowmodal(true);
+    setactiveID(id);
+  };
+
+  useEffect(() => {
+    if (selectTab === "all") {
+      setPortfolios(data);
+    }
+    if (selectTab === "web-design") {
+      const filteredData = data.filter(
+        (item) => item.category === "Web Design"
+      );
+      setPortfolios(filteredData);
+    }
+    if (selectTab === "ux-design") {
+      const filteredData = data.filter((item) => item.category === "Ux");
+      setPortfolios(filteredData);
+    }
+  }, [selectTab]);
+
   return (
     <section id="portfolio">
       <div className="container">
@@ -11,30 +43,63 @@ const Portfolio = () => {
             </h3>
           </div>
           <div className="flex gap-3">
-            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+            <button
+              onClick={() => setSelectTab("all")}
+              className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]"
+            >
               All
             </button>
-            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+            <button
+              onClick={() => setSelectTab("web-design")}
+              className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]"
+            >
               Web Design
             </button>
-            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+            <button
+              onClick={() => setSelectTab("ux-design")}
+              className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]"
+            >
               UI Design
-            </button>
-            <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
-              UX Design
             </button>
           </div>
         </div>
         <div className="flex items-center gap-4 flex-wrap mt-12">
-          {data?.map((portfolio, index) => (
-            <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]">
+          {portfolios?.slice(0, nextItem)?.map((portfolio, index) => (
+            <div
+              key={index}
+              data-aos="fade-zoom-in"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]"
+            >
               <figure>
-                <img className="rounded-[8]" src={portfolio.imgUrl} alt="" />
+                <img className="rounded-[8px]" src={portfolio.imgUrl} alt="" />
               </figure>
+              <div className="w-full h-full bg-primaryColor bg-opacity-40 absolute top-0 left-0 z-[5] hidden group-hover:block">
+                <div className="w-full h-full flex items-center justify-center">
+                  <button
+                    onClick={() => showmodalhandler(portfolio.id)}
+                    className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200"
+                  >
+                    See Details
+                  </button>
+                </div>
+              </div>
             </div>
           ))}
         </div>
+        <div className="text-center mt-6">
+          {nextItem < portfolios.length && data.length > 6 && (
+            <button
+              onClick={loadMoreHandler}
+              className="text-white bg-primaryColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200"
+            >
+              Load More
+            </button>
+          )}
+        </div>
       </div>
+      {showmodal && <Modal setShowModels={setshowmodal} activeID={activeID} />}
     </section>
   );
 };
